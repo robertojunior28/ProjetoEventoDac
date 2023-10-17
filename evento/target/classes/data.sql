61900d38e17ceb3bd9728cb6fc15c3c799a7f63a
José Roberto Júnior <112019067+robertojunior28@users.noreply.github.com>
2023-10-17 15:36:32 -0300

    gitignore

--- evento/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO user_model(email, name, password, user_role)
-VALUES ('admin@ifpb.com', 'Admin', '$2a$10$.piguPPbo5EtrcYe3lD8QeI59.Q1HfJk6V6Zu/zo82P.ZZ4Bi2.4i', 'ADMIN')
-    ON CONFLICT (email) DO NOTHING;
-
